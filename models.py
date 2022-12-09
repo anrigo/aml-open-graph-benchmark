5a066a74d7b7e3bae0dabb68c1db3904eec92baf
andrearigo-dev <andre.rg96@gmail.com>
2022-12-09 11:55:10 +0100

    EdgeGCN

--- models.py
+++ models.py
@@ -30,3 +30,37 @@ class GCN(torch.nn.Module):
         x = self.linear(x)
         x = self.sigmoid(x)
         return x
+
+
+class EdgeGCN(torch.nn.Module):
+    def __init__(self, in_channels, hidden_channels):
+        super().__init__()
+        self.atom_encoder = AtomEncoder(emb_dim=in_channels)
+        self.to_edge_weight = nn.Sequential(
+            nn.Linear(3, 1),
+            nn.ReLU
+        )
+        self.drop1 = nn.Dropout(p=0.5)
+        self.conv1 = GCNConv(in_channels, hidden_channels)
+        self.drop2 = nn.Dropout(p=0.5)
+        self.conv2 = GCNConv(hidden_channels, hidden_channels)
+        self.drop3 = nn.Dropout(p=0.5)
+        self.conv3 = GCNConv(hidden_channels, hidden_channels)
+        self.readout = aggr.MeanAggregation()
+        self.linear = nn.Linear(hidden_channels, 1)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x, edge_index, edge_weight=None, batch_idx=None):
+        x = self.atom_encoder(x)
+        # project edge features to edge weights > 1
+        edge_weight = self.to_edge_weight(edge_weight) + 1
+        x = self.drop1(x)
+        x = self.conv1(x, edge_index, edge_weight).relu()
+        x = self.drop2(x)
+        x = self.conv2(x, edge_index, edge_weight)
+        x = self.drop3(x)
+        x = self.conv3(x, edge_index, edge_weight)
+        x = self.readout(x, batch_idx)
+        x = self.linear(x)
+        x = self.sigmoid(x)
+        return x
