8b3dc2dbd66f6cf99ab80cbbbb4d8192fa01e8b1
andrearigo-dev <andre.rg96@gmail.com>
2022-12-10 16:04:23 +0100

    SimpleGIN

--- models.py
+++ models.py
@@ -1,7 +1,35 @@
 import torch
-from torch_geometric.nn import GCNConv, aggr
+from torch_geometric.nn import GCNConv, aggr, GINConv
 import torch.nn as nn
 from ogb.graphproppred.mol_encoder import AtomEncoder, BondEncoder
+
+
+class SimpleGIN(torch.nn.Module):
+    def __init__(self, in_channels, hidden_channels):
+        super().__init__()
+        self.atom_encoder = AtomEncoder(emb_dim=in_channels)
+        self.drop1 = nn.Dropout(p=0.5)
+        self.conv1 = GINConv(in_channels, hidden_channels)
+        self.drop2 = nn.Dropout(p=0.5)
+        self.conv2 = GINConv(hidden_channels, hidden_channels)
+        self.drop3 = nn.Dropout(p=0.5)
+        self.conv3 = GINConv(hidden_channels, hidden_channels)
+        self.readout = aggr.MeanAggregation()
+        self.linear = nn.Linear(hidden_channels, 1)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x, edge_index, edge_weight=None, batch_idx=None):
+        x = self.atom_encoder(x)
+        x = self.drop1(x)
+        x = self.conv1(x, edge_index).relu()
+        x = self.drop2(x)
+        x = self.conv2(x, edge_index).relu()
+        x = self.drop3(x)
+        x = self.conv3(x, edge_index).relu()
+        x = self.readout(x, batch_idx)
+        x = self.linear(x)
+        x = self.sigmoid(x)
+        return x
 
 
 class SimpleGCN(torch.nn.Module):
@@ -36,8 +64,7 @@ class EdgeGCN(torch.nn.Module):
     def __init__(self, in_channels, hidden_channels):
         super().__init__()
         self.atom_encoder = AtomEncoder(emb_dim=in_channels)
-        self.to_edge_weight = nn.Sequential(
-            BondEncoder(emb_dim=3), nn.Linear(3, 1))
+        self.to_edge_weight = nn.Linear(3, 1)
         self.drop1 = nn.Dropout(p=0.5)
         self.conv1 = GCNConv(in_channels, hidden_channels)
         self.drop2 = nn.Dropout(p=0.5)
