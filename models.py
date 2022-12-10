fcc0660e8c17e9d1c76faca859ea51e69ae01389
andrearigo-dev <andre.rg96@gmail.com>
2022-12-10 17:28:52 +0100

    Added residual

--- models.py
+++ models.py
@@ -7,30 +7,35 @@ from ogb.graphproppred.mol_encoder import AtomEncoder, BondEncoder
 class GCN(torch.nn.Module):
     def __init__(self, in_channels, hidden_channels, num_layers):
         super().__init__()
-        self.atom_encoder = AtomEncoder(emb_dim=in_channels)
+        self.atom_encoder = AtomEncoder(emb_dim=hidden_channels)
 
         self.num_layers = num_layers
-        self.drops = nn.ModuleList([
-            nn.Dropout(p=0.5)
-        ])
-        self.convs = nn.ModuleList([
-            GCNConv(in_channels, hidden_channels)
-        ])
+        self.drops = nn.ModuleList()
+        self.convs = nn.ModuleList()
+        self.norms = nn.ModuleList()
 
-        for _ in range(num_layers-1):
-            self.drops.append(nn.Dropout(p=0.5))
+        for _ in range(num_layers):
             self.convs.append(GCNConv(hidden_channels, hidden_channels))
+            self.norms.append(nn.BatchNorm1d(hidden_channels))
+            self.drops.append(nn.Dropout(p=0.5))
 
         self.readout = aggr.MeanAggregation()
         self.linear = nn.Linear(hidden_channels, 1)
         self.sigmoid = nn.Sigmoid()
 
-    def forward(self, x, edge_index, edge_weight=None, batch_idx=None):
+    def forward(self, x: torch.Tensor, edge_index, edge_weight=None, batch_idx=None):
         x = self.atom_encoder(x)
 
         for l in range(self.num_layers):
-            x = self.drops[l](x)
+            res = x.clone()
             x = self.convs[l](x, edge_index).relu()
+            x = self.norms[l](x)
+            # no relu on the last layer
+            if l < self.num_layers:
+                x = x.relu()
+            x = self.drops[l](x)
+            # residual connection
+            x += res
 
         x = self.readout(x, batch_idx)
         x = self.linear(x)
