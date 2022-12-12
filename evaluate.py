dc0fda88ce7263a3fc6a4c7456285568b67ef82d
andrearigo-dev <andre.rg96@gmail.com>
2022-12-12 16:09:50 +0100

    Test script

--- evaluate.py
+++ evaluate.py
@@ -1,6 +1,12 @@
+import argparse
+import os
+from pathlib import Path
 import torch
 from tqdm import tqdm
 from utils import accuracy
+from ogb.graphproppred import PygGraphPropPredDataset, Evaluator
+from torch_geometric.loader import DataLoader
+import models
 
 
 @torch.no_grad()
@@ -38,3 +44,55 @@ def eval(model, loader, evaluator, split=None, device=None):
     rocauc_dict = evaluator.eval(input_dict)
 
     return {f'{split}accuracy': acc, f'{split}rocauc': rocauc_dict['rocauc']}
+
+
+def test(args):
+    rundir = Path('runs', args.run)
+
+    if not rundir.exists():
+        raise FileNotFoundError('Run folder not found')
+    
+    checkpoints = [file_ for file_ in os.listdir(rundir) if '.pth' in file_]
+
+    if len(checkpoints) == 0:
+        raise FileNotFoundError('No checkpoint found')
+    
+    device = torch.device(
+        'cuda' if not args.cpu and torch.cuda.is_available() else 'cpu')
+
+    # Download and process data at './dataset/ogbg_molhiv/'
+    dataset = PygGraphPropPredDataset(name="ogbg-molhiv", root='dataset/')
+
+    split_idx = dataset.get_idx_split()
+    test_loader = DataLoader(dataset[split_idx["test"]], batch_size=args.batch_size, shuffle=False)
+    
+    evaluator = Evaluator(name="ogbg-molhiv")
+
+    for chk in checkpoints:
+        print(f'Evaluating {chk}')
+        model = models.GCN(dataset.num_features, args.emb_dim,
+                           args.layers, attnaggr=False).to(device)
+
+        state_dict = torch.load(Path(rundir, chk))
+        model.load_state_dict(state_dict)
+
+        metrics = eval(model, test_loader, evaluator, 'test', device)
+        print(metrics)
+
+
+if __name__ == '__main__':
+    # Evaluation settings
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--batch_size', type=int,
+                        default=64, help='batch size')
+    parser.add_argument('--emb_dim', type=int, default=300,
+                        help='dimensionality of hidden units in GNNs (default: 300)')
+    parser.add_argument('--layers', type=int, default=6,
+                        help='number of GNN layers (default: 6)')
+    parser.add_argument('--cpu', action=argparse.BooleanOptionalAction,
+                        default=False, help='run on cpu only')
+    parser.add_argument('--run', type=str, help='run name')
+
+    args = parser.parse_args()
+
+    test(args)
