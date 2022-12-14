f114f8f3644d40af1ab6233a465e8fe5650b79b0
andrearigo-dev <andre.rg96@gmail.com>
2022-12-14 15:13:25 +0100

    sagemax-attn

--- train.py
+++ train.py
@@ -41,7 +41,7 @@ def train(args):
     wandb.run.name = args.run
 
     model = models.SAGE(dataset.num_features, args.emb_dim,
-                           args.layers, aggrtype='attn', readout='mean').to(device)
+                           args.layers, aggrtype='max', readout='attn').to(device)
 
     optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
 
