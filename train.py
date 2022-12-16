bb5ab462b504c304b654c229e467f3e8f40a8291
andrearigo-dev <andre.rg96@gmail.com>
2022-12-16 09:51:53 +0100

    diffpool-attn-attn-pool

--- train.py
+++ train.py
@@ -41,7 +41,7 @@ def train(args):
     wandb.run.name = args.run
 
     model = models.DiffPool(dataset.num_features, args.emb_dim,
-                            args.layers, aggrtype='max', aggrpool='attn', readout='mean').to(device)
+                            args.layers, aggrtype='attn', aggrpool='attn', readout='pool').to(device)
 
     optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
 
